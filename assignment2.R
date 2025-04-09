ffa2a63ae3b5b71cb7188194762150c3ab691580
kaitlincastor <kncastor@gmail.com>
2025-04-09 12:55:23 -0700

    cleaned temperature and precipiration data

--- assignment2.R
+++ assignment2.R
@@ -8,22 +8,17 @@ library(here)
 climate_data_raw <- read.table(here("assignment 2", "clim.txt"), header = TRUE, sep = "", quote = "\"")
 source(here("R", "almondmodel.R"))
 
-# Select for 
-climate_data <- climate_data_raw |>
-  filter(month = 1 )
-
-# temperature data frame (code lines: 15-35)
-
-
-
+# temperature data frame (code lines: 15-30)
+temp_data <- climate_data_raw |> select(day, month, year, tmax_c, tmin_c) |> 
+  filter(month == 2)
 
 
 
 
 
 # precipitation data frame
-
-
+precip_data <- climate_data_raw |> select(day, month, year, precip) |> 
+  filter(month == 1)
 
 
 
