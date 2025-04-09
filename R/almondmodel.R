1310499c4f4e3ba8f26bf0f241134725ebbf99bb
kaitlincastor <kncastor@gmail.com>
2025-04-09 11:28:20 -0700

    adding the assignment, data, and function script

---
+++ R/almondmodel.R
@@ -0,0 +1,14 @@
+#assignment 2
+
+#almond yield anomaly function
+
+#' parameters
+#' Y = yield anomaly (ton / acre)
+#' Tn = minimum temperature (degrees C) -- month 2 (- = pre harvest)
+#' Tx = maximum temperature (degrees C)
+#' P = precipitation
+#' 
+
+
+
+Y = - 0.015 Tn2 - 0.0046Tn7**2 - 0.07P1 + 0.0043P1**2 + 0.28
