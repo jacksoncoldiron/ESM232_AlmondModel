532587f5af6540200c74a715b7ec0ffa208588d4
kaitlincastor <kncastor@gmail.com>
2025-04-09 11:46:26 -0700

    imported data

--- assignment2.R
+++ assignment2.R
@@ -1,1 +1,10 @@
 #assignment 2
+
+#import libraries
+library(tidyverse)
+library(here)
+
+#import data and function
+climate_data_raw <- read.csv(here("assignment 2", "clim.txt"))
+
+climate_data <- separate(climate_data_raw, sep = ".")
