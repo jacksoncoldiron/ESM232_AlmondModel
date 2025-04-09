df188f097d7ad51e22d84ca5132cf3071bcf86eb
jacksoncoldiron <jcoldiron@ucsb.edu>
2025-04-09 11:46:31 -0700

    function changes

--- R/almondmodel.R
+++ R/almondmodel.R
@@ -3,12 +3,18 @@
 #almond yield anomaly function
 
 #' parameters
-#' Y = yield anomaly (ton / acre)
-#' Tn = minimum temperature (degrees C) -- month 2 (- = pre harvest)
-#' Tx = maximum temperature (degrees C)
-#' P = precipitation
+#' @param Y = yield anomaly (ton / acre)
+#' @param Tn = minimum temperature (degrees C) -- month 2 (- = pre harvest)
+#' @param Tx = maximum temperature (degrees C)
+#' @param P = precipitation
 #' 
 
-
-
 Y = - 0.015 Tn2 - 0.0046Tn7**2 - 0.07P1 + 0.0043P1**2 + 0.28
+
+almonds <- function(Y, Tn, Tx, P) {
+  # Calculate the yield anomaly based on the given parameters
+  Y = -0.015 * Tn2 - 0.0046 * Tn2 - 0.07 * P + 0.0043 * P^2 + 0.28
+  
+  # Return the yield anomaly
+  return(Y)
+}
