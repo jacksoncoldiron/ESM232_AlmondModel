e216ad751a45a222684715a8aa6022cb64074120
kaitlincastor <kncastor@gmail.com>
2025-04-15 14:38:27 -0700

    added parameters

--- R/profit_function.R
+++ R/profit_function.R
@@ -1,5 +1,11 @@
 # Assignment 3
 
 # profit function
-# calls on the almond yield function
+# computes profit from almond yield, calling on the almond yield function
 
+#' parameters
+#' @param  price ($/kj)
+#' @param  energy (kj/yr)
+#' @param  year (when was almond yield)
+#' @param discount rate (default 0.12)
+#' @return data frame with estimate of profit
